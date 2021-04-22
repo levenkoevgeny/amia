2d27777a15f1f7fcaae3b0f538c7e5f31d735df9
Levenko Evgeny <levenko.evgeny@yandex.by>
2021-04-22 09:57:05 +0300

    fix

--- amia/settings.py
+++ amia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f1bi!_n#*7fr@dub40uf1$s@%#p7t#$y$b7lqbec^f%2q@z&1&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
